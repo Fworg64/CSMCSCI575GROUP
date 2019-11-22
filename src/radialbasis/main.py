6a731aa43a8bd4e1013e96fdb4840a67262c7bd7
Austin Oltmanns <aoltmanns@isengard.mines.edu>
2019-11-22 13:09:23 -0700

    added mask generator

--- src/radialbasis/main.py
+++ src/radialbasis/main.py
@@ -44,7 +44,7 @@ kernel_shape = (8, 8)
 offset = np.int((kernel_shape[0]) / 2)
 
 for idx, each_name in enumerate(image_files):
-    print(each_name)
+    print(each_name, end="  ")
     image = cv2.imread(mypath  + '/' + each_name)
     mask  = cv2.imread(mypath2 + '/' + each_name)
     # extend image by kernal_width/2
@@ -138,27 +138,24 @@ input_array = np.array(input_vector_list)
 output_array = np.array(output_class_list)
 
 x_train, x_test, y_train, y_test = train_test_split(
-                                      input_array, output_array, test_size=0.2, random_state=0)
+                                      input_array, output_array, test_size=0.4, random_state=0)
 
 print("Done loading")
 
 #kernel_input_shape = np.sum(my_kernel)
 kernel_input_shape = kernel_shape[0]*kernel_shape[1]
 layers = [
-     Dense(320,input_shape=(kernel_input_shape,), activation='sigmoid'),
-    # Dropout(0.3),
-#    Dense(640,input_shape=(640,), activation='elu'),
-#    Dropout(0.1),
-     Dense(320,input_shape=(320,), activation='sigmoid'),
-#    Dense(640,input_shape=(640,), activation='elu'),
-    #Dense(300,input_shape=(400,), activation='tanh'),
-     Dense(320,input_shape=(320,), activation='sigmoid'),
-     Dense(1,input_shape=(320,), activation='sigmoid'),
-    #Dense(1,input_shape=(10,), activation='tanh'),
+     Dense(8,input_shape=(kernel_input_shape,), activation='sigmoid'),
+     Dense(8, activation='sigmoid'),
+#     Dense(320,input_shape=(320,), activation='sigmoid'),
+     Dense(1, activation='sigmoid'),
+#     Dense(1,input_shape=(320,), activation='sigmoid'),
 ]
 model = keras.Sequential(layers)
 
-model.compile(optimizer='adam', loss='mse', metrics=['mae', "mse", "accuracy"])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[
+              "mse","mae", "binary_accuracy"])
+model.summary()
 model.fit(x_train, y_train, epochs=5, verbose=1)
 now = datetime. now()
 timestamp = datetime.timestamp(now)
@@ -167,16 +164,41 @@ model.summary()
 print("model saved to :model_file_" + str(timestamp) + ".h5")
 y_pred_float = model.predict(x_test)
 print(y_pred_float)
-y_class_pred = [1.0*(x>0.5) for x in y_pred_float]
+my_threshold = 0.9
+y_class_pred = [1.0*(x>my_threshold) for x in y_pred_float]
+#bin_acc = keras.metrics.binary_accuracy(y_test, y_pred_float, threshold=my_threshold)
 #y_class_pred = np.argmax(y_pred_float, axis=1)
-print("Decided %d salt pixels out of %d total pixels" %
-        (np.sum(y_class_pred), len(y_class_pred)))
-print(y_class_pred)
+print("Decided %d salt pixels out of %d total pixels with %d true salt" %
+        (np.sum(y_class_pred), len(y_class_pred), np.sum(y_test)))
+#print(y_class_pred)
 matthews_corr_coef = matthews_corrcoef(y_test, y_class_pred)
 print("Matthews Correlation Coef: %.4f" % (matthews_corr_coef,))
 scores = model.evaluate(x_test, y_test, verbose=0)
 print("FFNN Scores, mae, mae, mse, accuracy")
 print(scores)
+print("binary acc, confusion mat")
+num_correct = 0
+print("Comparing %d predictions with %d values" % (len(y_class_pred), len(y_test)))
+print("Calculating confusion matrix, C[pred][true]")
+confusion_mat = [[0,0],[0,0]]
+for index in range(0,len(y_class_pred)):
+    if (y_class_pred[index] == y_test[index]):
+        num_correct = num_correct + 1
+    confusion_mat[int(y_class_pred[index])][int(y_test[index])] = \
+            confusion_mat[int(y_class_pred[index])][int(y_test[index])] + 1 
+bin_acc = num_correct / len(y_test)
+#bin_acc = keras.metrics.binary_accuracy(y_test, y_pred_float, threshold=my_threshold)
+print(bin_acc)
+total_0_label = confusion_mat[0][0]+confusion_mat[0][1]
+correct_0_label = confusion_mat[0][0] / total_0_label
+incorrect_0_label = 1 - correct_0_label
+total_1_label = confusion_mat[1][0] + confusion_mat[1][1]
+correct_1_label = confusion_mat[1][1] / total_1_label
+incorrect_1_label = 1 - correct_1_label
+print("%d total pixels labeled 0. %.3f percent correct, %.3f percent incorrect" % \
+        (total_0_label, correct_0_label, incorrect_0_label))
+print("%d total pixels labeled 1. %.3f percent correct, %.3f percent incorrect" % \
+        (total_1_label, correct_1_label, incorrect_1_label))
 
 
 
